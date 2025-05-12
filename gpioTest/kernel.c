dd70373c335a61c22d94ea77577f2fbec6819fd7
Fermín Verdolini <fverdolini@oesia.com>
2025-05-12 21:50:26 +0200

    gpiotest

--- gpioTest/kernel.c
+++ gpioTest/kernel.c
@@ -1,4 +1,6 @@
 #include "gpio.h"
+#include "uart.h"
+#include <unistd.h>
 
 #define GPIO_PIN      22
 #define GPIO_PIN_MASK (1U << GPIO_PIN)
@@ -7,24 +9,14 @@ void delay(void) {
     for (volatile int i = 0; i < 10000; ++i) { }
 }
 
-void exit(int code) {
-    // syscall exit (for QEMU `-semihosting`)
-    asm volatile("mv a0, %0\n\t"
-                 "li a7, 93\n\t"
-                 "ecall"
-                 :
-                 : "r"(code)
-                 : "a0", "a7");
-    __builtin_unreachable();
-}
-
 void kmain(void) {
-    // Configura el pin como salida
+    uart_tx_enable();
+    uart_rx_enable();
+    print("GPIO test started\n");
     uint32_t dir = gpio_dir_read();
     dir |= GPIO_PIN_MASK;
     gpio_dir_write(dir);
 
-    // Escribe 1 y espera
     uint32_t val = gpio_read();
     val |= GPIO_PIN_MASK;
     gpio_write(val);
@@ -32,18 +24,19 @@ void kmain(void) {
 
     uint32_t input = gpio_read();
     if ((input & GPIO_PIN_MASK) == 0) {
-        exit(1); // Falla: no se lee alto
+        print("GPIO high test FAILED\n");
+        return;
     }
 
-    // Escribe 0 y espera
     val &= ~GPIO_PIN_MASK;
     gpio_write(val);
     delay();
 
     input = gpio_read();
     if (input & GPIO_PIN_MASK) {
-        exit(2); // Falla: no se lee bajo
+        print("GPIO low test FAILED\n");
+        return;
     }
 
-    exit(0); // Test OK
+    print("GPIO test PASSED\n");
 }
